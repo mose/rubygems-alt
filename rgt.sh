f98fe52d32f5511da1d679c74ede8d27d8c7bfe4
mose <mose@mose.com>
2013-08-17 00:45:09 +0800

    comments in the launcher

--- rgt.sh
+++ rgt.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# ./rgt.sh      : starts server
+# ./rgt.sh stop : stops server
+
 if [ "$1" = "stop" ]; then
   if test -f 'tmp/rubygems-announce.pid' ; then
     cat tmp/rubygems-announce.pid | xargs -- kill -9
